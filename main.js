e3474c83628636ae78a8543822715bd60fabfa12
SWASTIK SIBAM <89509745+GITHUBSHJ@users.noreply.github.com>
2022-05-01 21:21:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// all functions//
+//function preload() {}
+//function setup() {}
+//function draw() {}
+// all ending of functions// 
+
+console.log(
+	"%cNEXT CODING YOU WILL SEE IN PROJECT-124",
+	"color: white; background:linear-gradient(#E66465, #9198E5); padding: 1.2em; border-radius: 6px;"
+);
